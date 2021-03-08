0521671f6b5d5c7ca6b5b41bd2cb133425dc14ef
Middykins <kevinhamberg.jobb@gmail.com>
2021-03-08 16:23:08 +0100

    Nu kolliderar man bara med väggar!
    Signed-off-by: Middykins <kevinhamberg.jobb@gmail.com>

---
+++ CodePC/MainProgram/Communication.cpp
@@ -0,0 +1,23 @@
+#include "Communication.h"
+
+Communication::Communication()
+{
+}
+
+Communication::~Communication()
+{
+}
+
+void Communication::openMenu()
+{
+}
+
+void Communication::closeMenu()
+{
+}
+
+sf::Sprite Communication::getSprite()
+{
+	return sf::Sprite();
+}
+
